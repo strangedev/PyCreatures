90f268e0f85b21f0f9d358f060183b365a8c5c79
strangedev <strange.dev@gmail.com>
2015-12-01 13:34:47 +0100

    Addd methods for World

--- World.py
+++ World.py
@@ -12,16 +12,59 @@ class World(object):
     map_height = 24
 
     def __init__(self):
+
         self.world_map = {}
+        self.world_array = []
+
+        for x in range(self.map_height):
+            row = []
+
+            for y in range(self.map_width):
+                row.append(None)
+
+            self.world_array.append(row)
 
     def compute_life_cycle(self):
         pass
 
     def print_map(self):
-        pass
+        """
+        Map to ASCII method
+        """
+        map_str = ""
+
+        for x in range(self.map_height):
+            for y in range(self.map_width):
+
+                thing = self.get_thing(x, y)
 
     def get_pos(self, thing):
+        return self.world_map[thing]
+
+    def get_thing(self, x, y):
+        return self.world_array[x][y]
+
+    def move(self, thing, pos):
         pass
 
-    def move(self, this, pos):
+    def get_neighbor(self, thing, direction):
+        pass
+
+    def add_thing(self, thing, x, y):
+
+        # Keep reference of coords for thing
+        coords = (x, y)
+        key = thing.__hash__()
+
+        self.world_map[key] = coords
+        self.world_array[x][y] = thing
+
+    def remove_thing(self, x, y):
+
+        thing = self.get_thing(x, y)
+
+        self.world_array[x].pop(y)
+        del self.world_map[thing]
+
+    def get_neighbor_coords(self, x, y, direction):
         pass
